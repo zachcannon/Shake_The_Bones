0a53526fec97809ca038b92a3a747002be3e1a8b
Zachary Cannon <zach.cannon10@gmail.com>
2013-12-08 12:00:36 -0600

    Fixed up CSS features and added in responsive part of site.

--- app/static/js/instance.js
+++ app/static/js/instance.js
@@ -5,97 +5,99 @@ $( document ).ready(function() {
 	var currentShakerNumber = 0;
 	updateShaker(currentShakerNumber);
 
-	$('#reset_count_to_zero').click( function() {
-		$('#us_score_box').empty();	
-		$('#us_score_box').append(0)
+	$('.js-reset_count_to_zero').click( function() {
+		$('.js-us_score_box').empty();	
+		$('.js-us_score_box').append(0)
 		
-		$('#them_score_box').empty();	
-		$('#them_score_box').append(0)
+		$('.js-them_score_box').empty();	
+		$('.js-them_score_box').append(0)
 	});
 	
-	$('#us_win_the_turn').click( function() {
-		var current_turn_count = parseInt($('#count_on_turn').text(), 10);
-		var value = parseInt($('#us_score_box').text(), 10);
+	$('.js-us_win_the_turn').click( function() {
+		var current_turn_count = parseInt($('.js-count_on_turn:visible').text(), 10);
+		var value = parseInt($('.js-us_score_box:visible').text(), 10);
 		
 		value += current_turn_count
 		
-		$('#us_score_box').empty();	
-		$('#us_score_box').append(value);
+		$('.js-us_score_box').empty();	
+		$('.js-us_score_box').append(value);
 		
-		$('#count_on_turn').empty();
-		$('#count_on_turn').append(1);
+		$('.js-count_on_turn').empty();
+		$('.js-count_on_turn').append(1);
 		
 		currentShakerNumber = incrementShakerNumber(currentShakerNumber);
 		updateShaker(currentShakerNumber);
 	});
 	
-	$('#them_win_the_turn').click( function() {
-		var current_turn_count = parseInt($('#count_on_turn').text(), 10);
-		var value = parseInt($('#them_score_box').text(), 10);
+	$('.js-them_win_the_turn').click( function() {
+		var current_turn_count = parseInt($('.js-count_on_turn:visible').text(), 10);
+		var value = parseInt($('.js-them_score_box:visible').text(), 10);
 		
 		value += current_turn_count
 		
-		$('#them_score_box').empty();	
-		$('#them_score_box').append(value);
+		$('.js-them_score_box').empty();	
+		$('.js-them_score_box').append(value);
 		
-		$('#count_on_turn').empty();
-		$('#count_on_turn').append(1);
+		$('.js-count_on_turn').empty();
+		$('.js-count_on_turn').append(1);
 		
 		currentShakerNumber = incrementShakerNumber(currentShakerNumber);
 		updateShaker(currentShakerNumber);
 	});
 	
-	$('#next_shaker').click( function() {
+	$('.js-next_shaker').click( function() {
 		currentShakerNumber = incrementShakerNumber(currentShakerNumber);
 		updateShaker(currentShakerNumber);
 	});
 	
-	$('#previous_shaker').click( function() {
+	$('.js-previous_shaker').click( function() {
 		currentShakerNumber = decrementShakerNumber(currentShakerNumber);
 		updateShaker(currentShakerNumber);
 	});
 	
-	$('#increment_count').click( function() {
-		var current_turn_count = parseInt($('#count_on_turn').text(), 10);
+	$('.js-increment_count').click( function() {
+		var current_turn_count = parseInt($('.js-count_on_turn:visible').text(), 10);
 		current_turn_count += 1;		
-		$('#count_on_turn').empty();	
-		$('#count_on_turn').append(current_turn_count);
+		$('.js-count_on_turn').empty();	
+		$('.js-count_on_turn').append(current_turn_count);
 	});
 	
-	$('#decrement_count').click( function() {
-		var current_turn_count = parseInt($('#count_on_turn').text(), 10);
+	$('.js-decrement_count').click( function() {
+		var current_turn_count = parseInt($('.js-count_on_turn:visible').text(), 10);
 		current_turn_count -= 1;
 		if(current_turn_count < 1) current_turn_count = 1;		
-		$('#count_on_turn').empty();	
-		$('#count_on_turn').append(current_turn_count);
+		$('.js-count_on_turn').empty();	
+		$('.js-count_on_turn').append(current_turn_count);
 	});
 	
-	$('#us_increment').click( function() {
-		var value = parseInt($('#us_score_box').text(), 10);
+	$('.js-us_increment').click( function() {
+		var value = parseInt($('.js-us_score_box:visible').text(), 10);
 		value = value + 1;
-		$('#us_score_box').empty();	
-		$('#us_score_box').append(value);
+		$('.js-us_score_box').empty();	
+		$('.js-us_score_box').append(value);
 	});
 	
-	$('#us_decrement').click( function() {
-		var value = parseInt($('#us_score_box').text(), 10);
+	$('.js-us_decrement').click( function() {
+		var value = parseInt($('.js-us_score_box:visible').text(), 10);
 		value = value - 1;
-		$('#us_score_box').empty();	
-		$('#us_score_box').append(value);
+		if(value < 0) value=0;
+		$('.js-us_score_box').empty();	
+		$('.js-us_score_box').append(value);
 	});
 
-	$('#them_increment').click( function() {
-		var value = parseInt($('#them_score_box').text(), 10);
+	$('.js-them_increment').click( function() {
+		var value = parseInt($('.js-them_score_box:visible').text(), 10);
 		value = value + 1;
-		$('#them_score_box').empty();	
-		$('#them_score_box').append(value);
+		$('.js-them_score_box').empty();	
+		$('.js-them_score_box').append(value);
 	});
 	
-	$('#them_decrement').click( function() {
-		var value = parseInt($('#them_score_box').text(), 10);
+	$('.js-them_decrement').click( function() {
+		var value = parseInt($('.js-them_score_box:visible').text(), 10);
 		value = value - 1;
-		$('#them_score_box').empty();	
-		$('#them_score_box').append(value);
+		if(value < 0) value=0;
+		$('.js-them_score_box').empty();	
+		$('.js-them_score_box').append(value);
 	});
 });
 
@@ -112,21 +114,21 @@ function decrementShakerNumber(currentShakerNumber) {
 }
 
 function hideAllArrows() {
-	$('#arrow_up').hide();
-	$('#arrow_left').hide();
-	$('#arrow_right').hide();
-	$('#arrow_down').hide();
+	$('.js-arrow_up').hide();
+	$('.js-arrow_left').hide();
+	$('.js-arrow_right').hide();
+	$('.js-arrow_down').hide();
 }
 
 function updateShaker(currentShakerNumber) {
 	hideAllArrows();
 	if(currentShakerNumber == 0) {
-		$('#arrow_up').show();
+		$('.js-arrow_up').show();
 	} else if(currentShakerNumber == 1) {
-		$('#arrow_right').show();
+		$('.js-arrow_right').show();
 	} else if(currentShakerNumber == 2) {
-		$('#arrow_down').show();
+		$('.js-arrow_down').show();
 	} else {
-		$('#arrow_left').show();
+		$('.js-arrow_left').show();
 	}	
 }
