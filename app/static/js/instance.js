4f986e7bf3670984da033f168e7fd38c835dc849
Zachary Cannon <zach.cannon10@gmail.com>
2013-12-07 19:03:45 -0600

    Added shaker tracker functionality

--- app/static/js/instance.js
+++ app/static/js/instance.js
@@ -2,6 +2,19 @@
 
 $( document ).ready(function() {
 
+	var currentShakerNumber = 0;
+	updateShaker(currentShakerNumber);
+
+	$('#next_shaker').click( function() {
+		currentShakerNumber = incrementShakerNumber(currentShakerNumber);
+		updateShaker(currentShakerNumber);
+	});
+	
+	$('#previous_shaker').click( function() {
+		currentShakerNumber = decrementShakerNumber(currentShakerNumber);
+		updateShaker(currentShakerNumber);
+	});
+	
 	$('#us_increment').click( function() {
 		var value = parseInt($('#us_score_box').text(), 10);
 		value = value + 1;
@@ -30,3 +43,39 @@ $( document ).ready(function() {
 		$('#them_score_box').append(value);
 	});
 });
+
+function incrementShakerNumber(currentShakerNumber) {
+	currentShakerNumber ++;
+	if (currentShakerNumber > 3) currentShakerNumber = 0;
+	return currentShakerNumber;
+}
+
+function decrementShakerNumber(currentShakerNumber) {
+	currentShakerNumber --;
+	if (currentShakerNumber < 0) currentShakerNumber = 3;
+	return currentShakerNumber;
+}
+
+function updateShaker(currentShakerNumber) {
+	if(currentShakerNumber == 0) {
+		$('#arrow_up').show();
+		$('#arrow_left').hide();
+		$('#arrow_right').hide();
+		$('#arrow_down').hide();
+	} else if(currentShakerNumber == 1) {
+		$('#arrow_up').hide();
+		$('#arrow_left').hide();
+		$('#arrow_right').show();
+		$('#arrow_down').hide();
+	} else if(currentShakerNumber == 2) {
+		$('#arrow_up').hide();
+		$('#arrow_left').hide();
+		$('#arrow_right').hide();
+		$('#arrow_down').show();
+	} else {
+		$('#arrow_up').hide();
+		$('#arrow_left').show();
+		$('#arrow_right').hide();
+		$('#arrow_down').hide();
+	}	
+}
